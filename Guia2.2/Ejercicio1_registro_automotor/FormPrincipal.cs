c9e0465bf08829f6c4e71930e6eee533688594b2
fernandofilipuzzi-dev <fernandofilipuzzi.dev@gmail.com>
2025-08-26 13:40:55 -0300

    nota

---
+++ Guia2.2/Ejercicio1_registro_automotor/FormPrincipal.cs
@@ -0,0 +1,32 @@
+﻿using Ejercicio1.Models;
+
+namespace Ejercicio1;
+
+public partial class FormPrincipal : Form
+{
+    DepartamentoVehicular dpto = new DepartamentoVehicular();
+
+    public FormPrincipal()
+    {
+        InitializeComponent();
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+        Persona persona = new Persona(43402034,"Luisa");
+        RegistroVehiculo reg=dpto.RegistrarVehiculo(persona);
+                   
+        tbImpresoRegistro.Text=reg.VerDetalle();
+    }
+
+    private void button1_Click_1(object sender, EventArgs e)
+    {
+     
+        lbxRegistros.Items.Clear();
+        for (int idx = 0; idx < dpto.CantidadRegistros; idx++)
+        {
+            RegistroVehiculo reg = dpto.VerRegistro(idx);                
+            lbxRegistros.Items.Add(reg.VerDetalle());
+        }
+    }
+}
---
+++ Guia2.2/Ejercicio2_liquidacion_de_sueldos/FormRegistrarEmpleado.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Ejercicio2
+{
+    partial class FormRegistrarEmpleado
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tbApellido = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            tbNombre = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            groupBox1 = new GroupBox();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tbApellido
+            // 
+            tbApellido.Location = new Point(138, 27);
+            tbApellido.Margin = new Padding(4, 5, 4, 5);
+            tbApellido.Name = "tbApellido";
+            tbApellido.Size = new Size(148, 26);
+            tbApellido.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.DialogResult = DialogResult.OK;
+            button1.Location = new Point(28, 127);
+            button1.Margin = new Padding(4, 5, 4, 5);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 35);
+            button1.TabIndex = 1;
+            button1.Text = "Agregar";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.DialogResult = DialogResult.Cancel;
+            button2.Location = new Point(230, 127);
+            button2.Margin = new Padding(4, 5, 4, 5);
+            button2.Name = "button2";
+            button2.Size = new Size(112, 35);
+            button2.TabIndex = 2;
+            button2.Text = "Cancelar";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // tbNombre
+            // 
+            tbNombre.Location = new Point(138, 62);
+            tbNombre.Margin = new Padding(4, 5, 4, 5);
+            tbNombre.Name = "tbNombre";
+            tbNombre.Size = new Size(148, 26);
+            tbNombre.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(24, 30);
+            label1.Name = "label1";
+            label1.Size = new Size(65, 20);
+            label1.TabIndex = 4;
+            label1.Text = "Apellido";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(16, 65);
+            label2.Name = "label2";
+            label2.Size = new Size(73, 20);
+            label2.TabIndex = 5;
+            label2.Text = "Nombres";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(tbApellido);
+            groupBox1.Controls.Add(tbNombre);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(12, 13);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(351, 103);
+            groupBox1.TabIndex = 6;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Datos";
+            // 
+            // FormRegistrarEmpleado
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(372, 172);
+            Controls.Add(groupBox1);
+            Controls.Add(button1);
+            Controls.Add(button2);
+            Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Margin = new Padding(4, 5, 4, 5);
+            Name = "FormRegistrarEmpleado";
+            StartPosition = FormStartPosition.CenterParent;
+            Text = "Registrar Empleado";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.TextBox tbApellido;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        public System.Windows.Forms.TextBox tbNombre;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private GroupBox groupBox1;
+    }
+}
